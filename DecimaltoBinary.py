f1809d02bc6264e0e16acb3874bd1b0b8baac6fb
Vishal Rambilash Chauhan <138654751+Vishal-Chauhan07@users.noreply.github.com>
2023-10-28 11:55:32 +0530

    Add files via upload
    To convert a decimal number to binary number in python without using built-in function .
    This code defines a function toBinary that takes a decimal number as input and returns its binary representation divide the decimal number by 2 and record the remainder until the decimal number becomes 0 , effectively building the binary representation  in reverse order and then reversing  it to get the correct binary string

---
+++ DecimaltoBinary.py
@@ -0,0 +1,14 @@
+def toBinary(decimal):
+    Binary=" "
+    if decimal==0:
+        Binary=0
+    else:
+        while decimal>0:
+            remainder=decimal%2
+            Binary=str(remainder)+Binary
+            decimal=decimal//2
+    
+    return Binary
+decimal_number=eval(input("Enter the decimal number :"))
+display = toBinary(decimal_number)
+print(display)
